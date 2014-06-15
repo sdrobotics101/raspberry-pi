f10eee3b257fa551f4979eb4fdcda8af7d8c61c5
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2014-06-14 17:09:11 -0700

    Added new packet definitions
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- src/RXPacket.hpp
+++ src/RXPacket.hpp
@@ -9,8 +9,18 @@ public:
 private:
 	typedef struct
 	{
-		uint8_t test;
+		int16_t header;
+		int8_t acc_x;
+		int8_t acc_y;
+		int8_t acc_z;
+		int8_t mag_x;
+		int8_t mag_y;
+		int8_t mag_z;
+		int8_t pressure;
+		int8_t spare;
+		int16_t checksum;
 	}__attribute__((__packed__)) rx_packet_t;
+	int rx_packet_size;
 };
 
 #endif /* RXPacket_hpp */
--- src/TXPacket.hpp
+++ src/TXPacket.hpp
@@ -9,8 +9,20 @@ public:
 private:
 	typedef struct
 	{
-		uint8_t test;
+		int16_t header;
+		int8_t vel_x;
+		int8_t vel_y;
+		int8_t vel_z;
+		int8_t rot_x;
+		int8_t rot_y;
+		int8_t rot_z;
+		int16_t pos_z;
+		int8_t torpedo_ctl;
+		int8_t servo_ctl[6];
+		int8_t spare;
+		int16_t checksum;
 	}__attribute__((__packed__)) tx_packet_t;
+	int tx_packet_size;
 };
 
 #endif /* TXPacket_hpp */
