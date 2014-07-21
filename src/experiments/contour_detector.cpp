e792f5f915bbcd7c604742bfa22b917e82ad8e5a
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2014-07-21 00:07:59 -0700

    Running experiments on orange direction tape video
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- src/experiments/contour_detector.cpp
+++ src/experiments/contour_detector.cpp
@@ -17,9 +17,9 @@ int main(int argc, char *argv[])
 	cv::Mat hsv_thresh_hue_lower;
 	cv::Mat thresholded;
 	cv::threshold(img.hue, hsv_thresh_hue_lower, atoi(argv[2]), 255,
-		      CV_THRESH_BINARY);
+		      cv::THRESH_BINARY);
 	cv::threshold(img.hue, hsv_thresh_hue_upper, atoi(argv[3]), 255,
-		      CV_THRESH_BINARY_INV);
+		      cv::THRESH_BINARY_INV);
 	thresholded = hsv_thresh_hue_lower & hsv_thresh_hue_upper;
 	cv::Mat gray0;
 	thresholded.copyTo(gray0);
@@ -27,29 +27,29 @@ int main(int argc, char *argv[])
 	cv::Canny(gray0, gray, 0, atoi(argv[4]), 5);
 	cv::dilate(gray, gray, cv::Mat(), cv::Point(-1, -1));
 	std::vector < std::vector < cv::Point > >contours;
-	std::vector < cv::RotatedRect > boundingRects;
+	//std::vector < cv::RotatedRect > boundingRects;
 	cv::findContours(gray, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
-
-	for (uint i = 0; i < contours.size(); i++)	// Remove contours with area < 1000
-	{
-		if (cv::contourArea(contours.at(i)) < 1000) {
-			contours.erase(contours.begin() + i);
-			i--;
-		}
-	}
-	for (uint i = 0; i < contours.size(); i++)	// Compute minimum area bounding rectangles
-		boundingRects.push_back(cv::minAreaRect(contours.at(i)));
+	
+	/*for (uint i = 0; i < contours.size(); i++)	// Remove contours with area < 1000
+	  {
+	  if (cv::contourArea(contours.at(i)) < 1000) {
+	  contours.erase(contours.begin() + i);
+	  i--;
+	  }
+	  }
+	  for (uint i = 0; i < contours.size(); i++)	// Compute minimum area bounding rectangles
+	  boundingRects.push_back(cv::minAreaRect(contours.at(i)));*/
 	cv::Mat output;
 	img.rgb.copyTo(output);
 	for (uint i = 0; i < contours.size(); i++) {	// Show everything
-		cv::Point2f boundingPoints[4];
-		boundingRects.at(i).points(boundingPoints);
-		//std::cout << "Contour " << i << ": [";
-		cv::Scalar color(0, 0, 0);
-		//cv::drawContours(output, contours, i, color, 5, 8);
-		for (int i = 0; i < 4; i++)
+		cv::drawContours(output, contours, i, cv::Scalar(0, 0, 0), 5, 8);
+/*cv::Point2f boundingPoints[4];
+  boundingRects.at(i).points(boundingPoints);
+  //std::cout << "Contour " << i << ": [";
+  cv::Scalar color(0, 0, 0);
+  for (int i = 0; i < 4; i++)
 			cv::line(output, boundingPoints[i],
-				 boundingPoints[(i + 1) % 4], color);
+			boundingPoints[(i + 1) % 4], color);*/
 		//cv::imshow("Contour Detector", output);
 		//for (uint j = 0; j < contours.at(i).size(); j++) {
 		//std::cout << contours.at(i).at(j);
--- src/experiments/threshold_picker.cpp
+++ src/experiments/threshold_picker.cpp
@@ -5,7 +5,7 @@
 #include <opencv2/opencv.hpp>
 #include "../HSVImage.hpp"
 
-std::string window = "Color Follower";
+std::string window = "Threshold Picker";
 int hue_upper_threshold;
 std::string hue_upper_trackbar = "Hue Upper Threshold:";
 int hue_lower_threshold;
@@ -22,22 +22,37 @@ void trackbar_callback(int param, void *obj)
 	cv::Mat hsv_thresh_hue_lower;
 	cv::Mat thresholded;
 	cv::threshold(img.hsv, hsv_thresh_hue_lower, hue_lower_threshold, 255,
-		      CV_THRESH_BINARY);
+		      cv::THRESH_BINARY);
 	cv::threshold(img.hsv, hsv_thresh_hue_upper, hue_upper_threshold, 255,
-		      CV_THRESH_BINARY_INV);
+		      cv::THRESH_BINARY_INV);
 	thresholded = hsv_thresh_hue_lower & hsv_thresh_hue_upper;
-	cv::Mat gray0;
+	cv::imshow(window, thresholded);
+	/*cv::Mat gray0;
 	thresholded.copyTo(gray0);
 	cv::Mat gray;
 	cv::Canny(gray0, gray, 0, canny_threshold, 5);
 	cv::dilate(gray, gray, cv::Mat(), cv::Point(-1, -1));
 	std::vector < std::vector < cv::Point > >contours;
 	cv::findContours(gray, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
-	for (uint i = 0; i < contours.size(); i++) {
+	// This is for contour display
+	/*for (uint i = 0; i < contours.size(); i++) {
 		cv::Scalar color(0, 0, 0);
-		cv::drawContours(input, contours, i, color, 5, 8);
+		cv::drawContours(input, contours, i, color, 1, 8);
+		}
+
+	// This is for bounding rectangle displaye
+	std::vector<cv::RotatedRect> boundingRects;
+	for (uint i = 0; i < contours.size(); i++)
+		boundingRects.push_back(cv::minAreaRect(contours.at(i)));
+	for (uint i = 0; i < contours.size(); i++)
+	{
+		cv::Point2f boundingPoints[4];
+		boundingRects.at(i).points(boundingPoints);
+		for (int i = 0; i < 4; i++)
+			cv::line(input, boundingPoints[i], boundingPoints[(i + 1) % 4], cv::Scalar(0, 0, 0));
 	}
-	cv::imshow(window, input);
+
+	cv::imshow(window, input);*/
 }
 
 int main(int argc, char *argv[])
@@ -57,7 +72,10 @@ int main(int argc, char *argv[])
 	cv::createTrackbar(canny_trackbar, window, &canny_threshold, 255,
 			   trackbar_callback);
 	cv::imshow(window, raw_image);
-	cv::waitKey(0);
+	while (true)
+	{
+		cv::waitKey(0);
+	}
 
 	return EXIT_SUCCESS;
 }
