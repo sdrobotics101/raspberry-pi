97481ea9036d0e3a72848f1bb01f21f00f945ecc
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2014-07-22 14:43:30 -0700

    Wrote class to take care of logging
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

---
+++ src/Logger.cpp
@@ -0,0 +1,41 @@
+/* Logger.cpp -- Implementation of Logger class
+
+   Copyright (C) 2014 Tushar Pankaj
+   
+   This file is part of San Diego Robotics 101 Robosub.
+   
+   San Diego Robotics 101 Robosub is free software: you can redistribute it
+   and/or modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation, either version 3 of the License,
+   or (at your option) any later version.
+   
+   San Diego Robotics 101 Robosub is distributed in the hope that it will be
+   useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+   Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with San Diego Robotics 101 Robosub.  If not, see
+   <http://www.gnu.org/licenses/>. */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Logger.hpp"
+
+Logger::Logger(std::string filename, Logger::LogType input_min_log_type)
+{
+	log_file.open(filename);
+	min_log_type = input_min_log_type;
+}
+
+Logger::~Logger()
+{
+	log_file.close();
+}
+
+void Logger::write(std::string log, Logger::LogType type)
+{
+	if (type >= min_log_type)
+		log_file << log << std::endl;
+}
---
+++ src/Logger.hpp
@@ -0,0 +1,45 @@
+/* Logger.hpp -- Header file for Logger class
+
+   Copyright (C) 2014 Tushar Pankaj
+   
+   This file is part of San Diego Robotics 101 Robosub.
+   
+   San Diego Robotics 101 Robosub is free software: you can redistribute it
+   and/or modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation, either version 3 of the License,
+   or (at your option) any later version.
+   
+   San Diego Robotics 101 Robosub is distributed in the hope that it will be
+   useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+   Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with San Diego Robotics 101 Robosub.  If not, see
+   <http://www.gnu.org/licenses/>. */
+
+#ifndef Logger_hpp
+#define Logger_hpp
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class Logger {
+ public:
+	enum LogType {
+		ERROR,
+		WARNING,
+		MESSAGE,
+		VERBOSE,
+		DEBUG
+	};
+	 Logger(std::string filename, LogType input_min_log_type);
+	~Logger();
+	void write(std::string log, LogType type);
+ private:
+	 std::ofstream log_file;
+	LogType min_log_type;
+};
+
+#endif				// Logger_hpp
