496816097d3f0f825ffe3d201df2daac89371a40
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2014-07-27 13:52:54 -0700

    Added contour detection on value to experiments
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

---
+++ src/experiments/contour_detector_value.cpp
@@ -0,0 +1,93 @@
+/* contour_detector.cpp -- Experiment which thresholds on value
+
+   Copyright (C) 2014 Tushar Pankaj
+   
+   This file is part of San Diego Robotics 101 Robosub.
+   
+   San Diego Robotics 101 Robosub is free software: you can redistribute it
+   and/or modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation, either version 3 of the License,
+   or (at your option) any later version.
+   
+   San Diego Robotics 101 Robosub is distributed in the hope that it will be
+   useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+   Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with San Diego Robotics 101 Robosub.  If not, see
+   <http://www.gnu.org/licenses/>. */
+
+#include <stdlib.h>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "../HSVImage.hpp"
+
+int main(int argc, char *argv[])
+{
+	if (argc != 6) {
+		std::cerr << "Usage: " << argv[0] <<
+		    " [INPUT FILE] [VALUE LOWER THRESHOLD] [VALUE UPPER THRESHOLD] [CANNY THRESHOLD] [OUTPUT FILE]"
+		    << std::endl;
+		return EXIT_FAILURE;
+	}
+	cv::Mat raw_image = cv::imread(argv[1]);
+	HSVImage img = HSVImage(raw_image);
+	cv::Mat hsv_thresh_value_upper;
+	cv::Mat hsv_thresh_value_lower;
+	cv::Mat thresholded;
+	cv::threshold(img.value, hsv_thresh_value_lower, atoi(argv[2]), 255,
+		      cv::THRESH_BINARY);
+	cv::threshold(img.value, hsv_thresh_value_upper, atoi(argv[3]), 255,
+		      cv::THRESH_BINARY_INV);
+	thresholded = hsv_thresh_value_lower & hsv_thresh_value_upper;
+	cv::Mat gray0;
+	thresholded.copyTo(gray0);
+	cv::Mat gray;
+	cv::Canny(gray0, gray, 0, atoi(argv[4]), 5);
+	cv::dilate(gray, gray, cv::Mat(), cv::Point(-1, -1));
+	std::vector < std::vector < cv::Point > >contours;
+	std::vector < cv::RotatedRect > boundingRects;
+	cv::findContours(gray, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+
+	/*for (uint i = 0; i < contours.size(); i++)    // Remove contours with area < 1000
+	   {
+	   if (cv::contourArea(contours.at(i)) < 1000) {
+	   contours.erase(contours.begin() + i);
+	   i--;
+	   }
+	   } */
+	for (uint i = 0; i < contours.size(); i++)	// Compute minimum area bounding rectangles
+		boundingRects.push_back(cv::minAreaRect(contours.at(i)));
+	cv::Mat output;
+	img.rgb.copyTo(output);
+	for (uint i = 0; i < contours.size(); i++) {	// Show everything
+		//cv::drawContours(output, contours, i, cv::Scalar(0, 0, 0), 5, 8);
+		cv::Point2f boundingPoints[4];
+		boundingRects.at(i).points(boundingPoints);
+		//std::cout << "Contour " << i << ": [";
+		cv::Scalar color(0, 0, 0);
+		for (int i = 0; i < 4; i++)
+			cv::line(output, boundingPoints[i],
+				 boundingPoints[(i + 1) % 4], color);
+		//cv::imshow("Contour Detector", output);
+		//for (uint j = 0; j < contours.at(i).size(); j++) {
+		//std::cout << contours.at(i).at(j);
+		//if (j != contours.at(i).size() - 1)
+		//std::cout << ", ";
+		//}
+		//std::cout << "], Bounding: ";
+		//for (int j = 0; j < 4; j++) {
+		//      std::cout << boundingPoints[j];
+		//      if (j != 3)
+		//                      std::cout << ", ";
+		//}
+		//std::cout << std::endl;
+	}
+	cv::imwrite(argv[5], output);
+	/*cv::namedWindow("CV", 0);
+	   cv::imshow("CV", thresholded);
+	   cv::waitKey(0); */
+
+	return EXIT_SUCCESS;
+}
